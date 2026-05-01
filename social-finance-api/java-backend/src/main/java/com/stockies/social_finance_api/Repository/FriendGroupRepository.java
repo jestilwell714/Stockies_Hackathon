4e1b5bc91246496fcab82cac9dfdd5cad9c106d3
jamesstilwell <jamesstilwelll@icloud.com>
2026-05-02 02:31:05 +1200

    Done interface for FriendGroupRepository

---
+++ social-finance-api/java-backend/src/main/java/com/stockies/social_finance_api/Repository/FriendGroupRepository.java
@@ -0,0 +1,10 @@
+package com.stockies.social_finance_api.Repository;
+
+import com.stockies.social_finance_api.entity.FriendGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FriendGroupRepository extends JpaRepository<FriendGroup, Long> {
+    Optional<FriendGroup> findByInviteCode(String inviteCode);
+}
