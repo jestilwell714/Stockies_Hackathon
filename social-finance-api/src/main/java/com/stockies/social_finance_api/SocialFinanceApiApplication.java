300d382002290afb8354e7b52ecd1c931059ccc1
jamesstilwell <jamesstilwelll@icloud.com>
2026-05-02 00:37:27 +1200

    Initial commit

---
+++ social-finance-api/src/main/java/com/stockies/social_finance_api/SocialFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.stockies.social_finance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialFinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialFinanceApiApplication.class, args);
+	}
+
+}
---
+++ social-finance-api/src/test/java/com/stockies/social_finance_api/SocialFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockies.social_finance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
