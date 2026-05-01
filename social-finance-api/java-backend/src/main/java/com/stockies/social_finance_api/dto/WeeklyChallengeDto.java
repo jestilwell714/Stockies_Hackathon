2e8f2b0e01c0879e17d2861e9e59492e2df25843
jamesstilwell <jamesstilwelll@icloud.com>
2026-05-02 03:03:35 +1200

    Lombok annotations

--- social-finance-api/java-backend/src/main/java/com/stockies/social_finance_api/dto/WeeklyChallengeDto.java
+++ social-finance-api/java-backend/src/main/java/com/stockies/social_finance_api/dto/WeeklyChallengeDto.java
@@ -1,7 +1,12 @@
 package com.stockies.social_finance_api.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
+@Builder
 public class WeeklyChallengeDto {
     private Long id;
     private LocalDate startDate; // The Sunday it snapped to
