72efcc55e4a0250d7c9561fa9ca0c56214264942
jamesstilwell <jamesstilwelll@icloud.com>
2026-05-02 02:21:42 +1200

    Removed unnecassary categories

--- social-finance-api/java-backend/src/main/java/com/stockies/social_finance_api/entity/WeeklyChallenge.java
+++ social-finance-api/java-backend/src/main/java/com/stockies/social_finance_api/entity/WeeklyChallenge.java
@@ -1,8 +1,8 @@
 package com.stockies.social_finance_api.entity;
-
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "weekly_challenges")
@@ -16,17 +16,10 @@ public class WeeklyChallenge {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String challengeName;
-
-    private String targetCategory;
-
     @Column(nullable = false)
     private LocalDate startDate;
 
-    @Column(nullable = false)
     private LocalDate endDate;
-
-    private Integer pointPrize;
 
     private Boolean isActive = true;
 }
