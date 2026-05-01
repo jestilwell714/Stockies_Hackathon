2a4e264890147b32e3c271d61567207d826e61b8
jamesstilwell <jamesstilwelll@icloud.com>
2026-05-02 01:48:04 +1200

    user entity done

---
+++ social-finance-api/java-backend/src/main/java/com/stockies/social_finance_api/entity/User.java
@@ -0,0 +1,53 @@
+package com.stockies.social_finance_api.entity;
+
+import jakarta.persistence.*;
+import jakarta.transaction.Transaction;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+
+@Data
+
+@NoArgsConstructor
+
+@AllArgsConstructor
+
+@Table(name = "users")
+
+public class User {
+
+
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+
+
+    @Column(nullable = false, unique = true)
+
+    private String username;
+
+
+    @Column(nullable = false)
+
+    private Integer totalPoints = 0;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+
+    @JoinColumn(name = "group_id")
+
+    private FriendGroup friendGroup;
+
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+
+    private List<Transaction> transactions;
+
+}
