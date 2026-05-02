0407ab2b80fea435f12f1ea1e05c31e568771d9e
jamesstilwell <jamesstilwelll@icloud.com>
2026-05-02 23:34:39 +1200

    Method need to be in interface

--- social-finance-api/java-backend/src/main/java/com/stockies/social_finance_api/service/UserService.java
+++ social-finance-api/java-backend/src/main/java/com/stockies/social_finance_api/service/UserService.java
@@ -2,6 +2,9 @@ package com.stockies.social_finance_api.service;
 
 import com.stockies.social_finance_api.dto.UserDto;
 
+import java.util.List;
+
 public interface UserService {
-    UserDto createUser(UserDto dto);
+    public UserDto createUser(UserDto dto);
+    public List<UserDto> getAllUsers();
 }
