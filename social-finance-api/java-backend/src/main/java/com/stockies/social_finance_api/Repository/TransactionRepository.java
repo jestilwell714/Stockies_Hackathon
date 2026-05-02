b41f0dcfd241e005ed4ed3b3027b55dd01ec5178
jamesstilwell <jamesstilwelll@icloud.com>
2026-05-02 12:08:04 +1200

    Description not category

--- social-finance-api/java-backend/src/main/java/com/stockies/social_finance_api/Repository/TransactionRepository.java
+++ social-finance-api/java-backend/src/main/java/com/stockies/social_finance_api/Repository/TransactionRepository.java
@@ -3,10 +3,13 @@ package com.stockies.social_finance_api.Repository;
 import com.stockies.social_finance_api.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     List<Transaction> findByUserIdAndTimestampBetween(Long userId, LocalDateTime start, LocalDateTime end);
+    Optional<Transaction> findByUserIdAndDescription(Long userId, String description);
 }
